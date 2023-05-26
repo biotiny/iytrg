3be398c78eb69f30b336558daac16efa2f193e0d
biotiny <133472350+biotiny@users.noreply.github.com>
2023-05-26 02:14:49 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-5a40e39ad0a34a968d376246b67ac52b}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
